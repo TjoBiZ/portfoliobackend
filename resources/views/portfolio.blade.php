342fa62bc779d6c8dca8fc113536eeefa8543c72
Joker <j@tjo.biz>
2024-05-03 16:53:31 -0700

    Add Google TAG manager

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -25,6 +25,13 @@
     <link rel="stylesheet" href="css/main.css" type="text/css">
     <script src="js/jquery-2.1.3.min.js"></script>
     <script src="js/modernizr.custom.js"></script>
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K3P93PKD');</script>
+      <!-- End Google Tag Manager -->
   </head>
 
   <body>
@@ -1026,5 +1033,9 @@ Achievements:
     <script src="js/tilt.jquery.min.js"></script>
     <script src="js/jquery.hoverdir.js"></script>
     <script src="js/main.js"></script>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3P93PKD"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
   </body>
 </html>
