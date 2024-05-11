2e59ccd9610d8c14c725620c9942dd3295bc1c2a
Joker <j@tjo.biz>
2024-05-11 13:32:40 -0700

    Delete Google Tag Manager Add Google Analitycs version 4

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -25,13 +25,15 @@
     <link rel="stylesheet" href="css/main.css" type="text/css">
     <script src="js/jquery-2.1.3.min.js"></script>
     <script src="js/modernizr.custom.js"></script>
-      <!-- Google Tag Manager -->
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-K3P93PKD');</script>
-      <!-- End Google Tag Manager -->
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-C02J53TSVF"></script>
+      <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-C02J53TSVF');
+      </script>
   </head>
 
   <body>
@@ -1033,9 +1035,5 @@ Achievements:
     <script src="js/tilt.jquery.min.js"></script>
     <script src="js/jquery.hoverdir.js"></script>
     <script src="js/main.js"></script>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3P93PKD"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
   </body>
 </html>
