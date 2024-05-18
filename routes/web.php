b35effc1d3d6d4afec8788c753b8d715df5cf105
vagrant <vagrant@homestead>
2024-05-18 15:56:18 -0500

    Integration Laravel Breeze Auth Design for login form

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
 
+use App\Http\Controllers\ProfileController;
+
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\ContactFormController;
@@ -24,3 +26,15 @@ Route::post('/contact-form', function (Request $request) {
 Route::get('/laravel', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
