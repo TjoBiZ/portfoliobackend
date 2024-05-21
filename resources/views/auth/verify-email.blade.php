7054e871dff48956d74f3a231a7dc7441be00d8f
vagrant <vagrant@homestead>
2024-05-20 20:04:26 -0500

    Add information for check SPAM folder aftre regestration

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,18 +1,18 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-neutral-300">
         @if(session('email_verification'))
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another. ') }} {{ 'Check your email: ' . session('email_verification') }}
+            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another. ') }} {{ 'Check your email in SPAM folder if you do not find it in your inbox. Your email address is ' . session('email_verification') }}
         @else
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another. ') }}
+            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another. Check your email in SPAM folder if you do not find it in your inbox.') }}
         @endif
     </div>
 
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600">
             @if(session('email_verification'))
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }} {{ 'Check your email: ' . session('email_verification') }}
+                {{ __('A new verification link has been sent to the email address you provided during registration.') }} {{ 'Check your email in SPAM folder if you do not find it in your inbox. Your email address is: ' . session('email_verification') }}
             @else
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                {{ __('A new verification link has been sent to the email address you provided during registration. Check your email in SPAM folder if you do not find it in your inbox.') }}
             @endif
         </div>
     @endif
