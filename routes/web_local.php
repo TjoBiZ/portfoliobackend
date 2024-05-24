4291e0303895ed67d35921619fa0620b5601e4d9
vagrant <vagrant@homestead>
2024-05-24 12:38:48 -0500

    Add different route for Local Development envierement and production. Different files for symbolink

---
+++ routes/web_local.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\Http\Controllers\Auth\Http\Controllers\Http\Controllers\ContactFormController;
+use App\Http\Controllers\Http\Controllers\Auth\Http\Controllers\Http\Controllers\ProfileController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Http\Middleware\CustomEnsureEmailIsVerified;
+use Illuminate\Support\Facades\Route;
+
+// Группа маршрутов для домена portfolio.local
+Route::domain('portfolio.local')->group(function () {
+    Route::get('/', function () {
+        return view('portfolio');
+    });
+
+    Route::get('/video', function () {
+        return view('video');
+    });
+
+    Route::post('/contact-form', function (Request $request) {
+        return app(ContactFormController::class)->sendMessage($request);
+    });
+});
+
+Route::domain('admin.portfolio.local')->group(function () {
+    Route::get('/laravel', function () {
+        return view('welcome');
+    });
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    require __DIR__.'/auth.php';
+});
